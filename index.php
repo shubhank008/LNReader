83ab622ef257072885e3f311ddb10d881a4d16bc
shubhank008 <shubhank008pp@gmail.com>
2016-01-10 00:02:26 +0530

    Improved Description/Image/Synopsis scrapping
    --Using managupdates for scraping synopsis
    --Added fallback method for getting description
    --Changed image scrapping method
    
    --Added mangaupdates.php helper file
    --Made functions public in functions.php for testing

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
+<pre><?php
 require_once("./functions.php");
 $ABC = new LNScrape();
-$ABC->Scrape();
-?>
+$out=$ABC->GetLN();
+$i=0;
+foreach($out as $LN) {
+//$out = $ABC->getDescForTitle($LN);
+//$out = str_replace("\"","'",getMUSynopsisForTitle($LN));
+//$out = $ABC->getImageForTitle($LN);
+print_r($out);
+$i++;
+if($i==30){break;}
+}
+
+//print_r($out);
+?></pre>
